c8931057eea828d9311f7a87bb2a32a4a65cca26
Jeffrey Hata <67081225+Jeff591@users.noreply.github.com>
2025-10-11 21:55:48 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple fade-in effect on scroll
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('section').forEach(sec => observer.observe(sec));
